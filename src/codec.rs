a5d41d20752610a80c4446cdd329a81728115c0e
Geoff Clements <ro2kz0@gmail.com>
2023-07-11 18:10:36 +0100

    Update to documentation

--- src/codec.rs
+++ src/codec.rs
@@ -1,29 +1,24 @@
-/*
-Traits used by the user-defined decoders and encoders.
-These are used with [`FramedRead`] and [`FramedWrite`]
-*/
+//! Traits used by the user-defined decoders and encoders.
+//! These are used with [`FramedRead`] and [`FramedWrite`]
 
 use bytes::BytesMut;
 use std::io;
 
-/*
- * The `Decoder` trait.
- * Objects that implement this trait take a `BytesMut` and return
- * an item whose type is defined as the associated type `Item`.
- * 
- * It is important that implementers deplete bytes from `src` when
- * they are used to form the returned `item`.
- * 
- * There are three possibilities to return from this method:
- * - `Ok(Some(Item))` - in which case decoding is complete and the
- * user defined `item` is returned.
- * - `Ok(None)`- there is not enough data in `src` to decode the
- * message. When this is returned `read` is called again on the
- * underlying `Read` object.
- * - `Err()` - An error has occurred. This will close the underlying
- * `Read` object. If you want to indicate a protocol error it is better
- * to use the user defined `Item` to do this.
- */
+/// The `Decoder` trait.
+/// Objects that implement this trait take a `BytesMut` and return
+/// an item whose type is defined as the associated type `Item`.
+/// 
+/// It is important that implementers deplete bytes from `src` when
+/// they are used to form the returned `item`.
+/// There are three possibilities to return from this method:
+/// - `Ok(Some(Item))` - in which case decoding is complete and the
+/// user defined `item` is returned.
+/// - `Ok(None)`- there is not enough data in `src` to decode the
+/// message. When this is returned `read` is called again on the
+/// underlying `Read` object.
+/// - `Err()` - An error has occurred. This will close the underlying
+/// `Read` object. If you want to indicate a protocol error it is better
+/// to use the user defined `Item` to do this.
 pub trait Decoder {
     type Item;
     type Error: From<io::Error>;
@@ -31,11 +26,9 @@ pub trait Decoder {
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error>;
 }
 
-/*
- * The `Encoder` trait.
- * Objects that implement this trait take a user-defined `Item` and
- * insert into the provided`BytesMut`.
- */
+ /// The `Encoder` trait.
+ /// Objects that implement this trait take a user-defined `Item` and
+ /// insert into the provided`BytesMut`.
 pub trait Encoder<I> {
     type Error: From<io::Error>;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,56 @@
-/*
-# Framous
-
-This package is inspired by the `codec` module from [`tokio::util`] but, unlike tokio,
-is designed to work with non-async code.
-
-The intended use case for this crate is when you need to send and receive frames of
-data via some add-hoc byte-orientated protocol, usually but not necessarily, over TCP.
-
-- It supports the sending of user-defined message structures by encoding them to a
-byte-orientated frame through a user-defined `Encoder`.
-
-- Conversely, it supports the receiving of a byte-oriented frames and decoding then through
-a user-defined `Decoder` into messages as understood by the application.
-
-
-[`tokio::util`]: https://docs.rs/tokio-util/latest/tokio_util/
-*/
+//! # Framous
+//! This package is inspired by the `codec` module from [`tokio::util`] but, unlike tokio,
+//! is designed to work with non-async code.
+//! 
+//! The intended use case for this crate is when you need to send and receive frames of
+//! data via some add-hoc byte-orientated protocol, usually but not necessarily, over TCP.
+//! 
+//! - It supports the sending of user-defined message structures by encoding them to a
+//! byte-orientated frame through a user-defined `Encoder`.
+//! 
+//! - Conversely, it supports the receiving of a byte-oriented frames and decoding then through
+//! a user-defined `Decoder` into messages as understood by the application.
+//! 
+//! [`tokio::util`]: https://docs.rs/tokio-util/latest/tokio_util/
+//! 
+//! Typical usage:
+//! ```no-run
+//! 
+//! enum MyMessage {
+//!     msg1,
+//!     msg2,
+//! }
+//! 
+//! struct MyCodec;
+//! 
+//! impl Decoder for MyCodec {
+//!     type Item = MyMessage;
+//!     type Error = io::Error;
+//! 
+//!     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+//!         // decode code
+//!         // see [`codec::Decoder`]
+//!     }
+//! }
+//! 
+//! impl Encoder<MyMessage> for TestCodec {
+//!     type Error = io::Error;
+//! 
+//!     fn encode(&mut self, item: MyMessage, dst: &mut BytesMut) -> Result<(), Self::Error> {
+//!         // encode code
+//!         // see [`codec::Encoder`]
+//!     }
+//! }
+//! 
+//! let cx = TcpStream::connect("127.0.0.1:35642").unwrap();
+//! let mut rx = FramedRead(cx.try_clone()?, MyCodec);
+//! let mut tx = FramedWrite(cx, MyCodec);
+//! 
+//! // Send a message
+//! tx.framed_write(MyMessage::msg1).unwrap();
+//! 
+//! // Block on waiting for a message
+//! let msg = rx.framed_read().unwrap();
 
 pub mod codec;
 pub mod framed;
